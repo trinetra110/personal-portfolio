6036e271f4f02fbd17d605c037c43d1f2ce3342c
trinetra110 <ayanmondal1805@gmail.com>
2024-11-27 01:32:44 +0530

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal-portfolio",
   plugins: [react()],
   server: {
     // Set port and open browser automatically (optional)
