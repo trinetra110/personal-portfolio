e69c1f0ab1696f8f5ce1a3611d9dc8e13ce8540f
trinetra110 <ayanmondal1805@gmail.com>
2024-12-10 12:52:21 +0530

    testing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal-portfolio",
+  base: "/",
   plugins: [react()],
   server: {
     // Set port and open browser automatically (optional)
